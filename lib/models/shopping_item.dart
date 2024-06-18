a38be7db46ba001a6da7fc9dcb297fb3dc078df1
Débora Rodrigues <81527964+souzadbr@users.noreply.github.com>
2024-06-18 13:10:35 -0300

    Definir model para um item

---
+++ lib/models/shopping_item.dart
@@ -0,0 +1,13 @@
+class ShoppingItem {
+  final String name;
+  final int quantity;
+  final double price;
+
+  ShoppingItem({
+    required this.name,
+    required this.quantity,
+    required this.price,
+  });
+}
+//models/shopping_item.dart: Define a classe ShoppingItem.
+
